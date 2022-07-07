69c523aa02603016ce58102adb041d90d83533d3
Emilys-Air <em.smolka@gmail.com>
2022-07-07 06:39:48 -0400

    start page css

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,18 @@
+var start = document.querySelector('#start-quiz')
+var questions = document.querySelector('.questions')
+var highScores = document.querySelector(".high-scores")
 
+var questionHeader = document.querySelector(".question-text")
+var answer1 = document.querySelector("#option-a")
+var answer2 = document.querySelector("#option-b")
+var answer3 = document.querySelector("#option-c")
+var answer4 = document.querySelector("#option-d")
 
 const timeValue = document.querySelector('#time');
 //const timerTime = new timeValue(0).getTime();
 var timerCount
 var stopwatch 
-
+var questionIndex = 0
 let question = [
 
     {question: "Here is the text for the question 1",
@@ -23,22 +31,34 @@ function countUp(){
     //const secValue = Math.floor(difference/(1000) % 60 );
     timeValue.innerHTML = timerCount;
     //secValue.innerHTML = secValue;
-    timerCount++;
+    timerCount--;
 }
 
 function beginQuiz() {
 
     // Get the timer started
-    timerCount = 0
+    timerCount = 75
     stopwatch = setInterval(countUp, 1000);
 
     // Hide the starting/home section
-    document.querySelector('#start-quiz').style.display="none";
+    start.style.display="none";
 
     // Show the questions section...
-    document.querySelector('.questions').style.display="block";
+    questions.style.display="block";
+
+    displayQuestions ()  
 
 }
+function displayQuestions(){
+questionHeader.textContent = question[questionIndex].question
+answer1.textContent = question[questionIndex].answers[0]
+answer2.textContent = question[questionIndex].answers[1]
+answer3.textContent = question[questionIndex].answers[2]
+answer4.textContent = question[questionIndex].answers[3]
+}
 
+//code quiz answer question move page with array of objects, click and move page
+//incerase question index by one, display questions again (will be a while loop)
+//if timer reaches zero or if index = question.length end quiz and submit initals and display high scores 
 // Add click event listener to start button
 document.getElementById("start").addEventListener("click", beginQuiz);
